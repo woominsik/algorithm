e24d4a19ddef0d63da51b0698dcd49eefe1290e1
woominsik <9737638@naver.com>
2022-06-25 16:22:37 +0900

    "22-06-25"

--- src/BaekJoon/Main.java
+++ src/BaekJoon/Main.java
@@ -1,78 +1,54 @@
 package BaekJoon;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 import java.util.Stack;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n= sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Stack<Integer>stack = new Stack<>();
         for(int i=0;i<n;i++){
-            String temp = sc.next();
-            System.out.println(compute(temp));
-        }
-    }
-    static int compute(String s){
-//        StringBuilder right = new StringBuilder();
-//        StringBuilder left = new StringBuilder();
-        String right="";
-        String left="";
-        if(s.length()%2==1){
-            right=s.substring(0,s.length()/2+1);
-            left=s.substring(s.length()/2+1);
-        }
-        else{
-            right=s.substring(0,s.length()/2);
-            left=s.substring(s.length()/2);
+            String[] temp = br.readLine().split(" ");
+            switch(temp[0]){
+                case "push":
+                    stack.push(Integer.parseInt(temp[1]));
+                    break;
+                case "top":
+                    if(stack.isEmpty()){
+                        System.out.println(-1);
+                    }
+                    else{
+                        System.out.println(stack.peek());
+                    }
+                    break;
+
+                case "pop":
+                    if(stack.isEmpty()){
+                        System.out.println(-1);
+                    }
+                    else{
+                        System.out.println(stack.pop());
+                    }
+                    break;
+
+                case "size":
+                    System.out.println(stack.size());
+                    break;
+
+                case "empty":
+                    if(stack.isEmpty()){
+                        System.out.println(1);
+                    }
+                    else{
+                        System.out.println(0);
+                    }
+                    break;
+            }
         }
 
-//        for(int i=0;i<s.length();i++){
-//            int front=i;
-//            int back = s.length()-1-i;
-//            if(front>=back){
-//                break;
-//            }
-//            right.append(s.charAt(front));
-//            left.append(s.charAt(back));
-//        }
-//        if(s.length()%2==1){
-//            right.append(s.charAt(s.length()/2+1));
-//        }
-        return check(right,left);
-    }
-    static int check(String right, String left){
-        System.out.println(right+" "+left);
-        int count=0;
-        int ri=0;
-        int li=0;
-        while(true){
-//            System.out.println(ri+" "+li);
-            if(ri>=right.length()||li>=left.length()){
-                break;
-            }
-            if(right.charAt(ri)==left.charAt(li)){
-                ri++;
-                li++;
-            }
-            else{
-                if(right.length()-ri>left.length()-li){
-                    ri++;
-                    count++;
-                }
-                else{
-                    li++;
-                    count++;
-                }
-            }
-        }
-        if(count==0){
-            return 0;
-        }
-        else if(count==1){
-            return 1;
-        }
-        else{
-            return 2;
-        }
     }
 }
---
+++ src/BaekJoon/Problem_10828.java
@@ -0,0 +1,4 @@
+package BaekJoon;
+
+public class Problem_10828 {
+}
