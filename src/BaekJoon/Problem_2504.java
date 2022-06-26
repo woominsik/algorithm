bb34f28e7cedd5936c5fecbd5aa5962fa45e8044
woominsik <9737638@naver.com>
2022-06-27 00:00:23 +0900

    "22-06-27"

--- src/BaekJoon/Problem_2504.java
+++ src/BaekJoon/Problem_2504.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
 
-public class Main {
+public class Problem_2504 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String temp = br.readLine();
