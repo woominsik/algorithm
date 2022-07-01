d19080835299a3faa69c8053046b634bf46cb6a5
woominsik <9737638@naver.com>
2022-07-01 19:05:12 +0900

    "22-07-01"

--- src/BaekJoon/Problem_11286.java
+++ src/BaekJoon/Problem_11286.java
@@ -1,17 +1,15 @@
 package BaekJoon;
 
-
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.TreeMap;
 
-
-
-class Main{
+public class Problem_11286 {
     static TreeMap<Integer, ArrayList<Integer>> absNum;
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         absNum = new TreeMap<>();
@@ -49,49 +47,3 @@ class Main{
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//
-//class Main {
-//    public static void main(String[] args) {
-//        전사 a전사 = new 전사();
-//
-//        a전사.공격(/* 수정가능지역 시작 */"브라이언","칼"/* 수정가능지역 끝 */);
-//        // 브라이언이(가) 칼(으)로 공격합니다.
-//
-//        a전사.공격(/* 수정가능지역 시작 */"필립","창" /* 수정가능지역 끝 */);
-//        // 필립이(가) 창(으)로 공격합니다.
-//
-//        a전사.공격(/* 수정가능지역 시작 */"마크","지팡이" /* 수정가능지역 끝 */);
-//        // 마크(가) 지팡이(으)로 공격합니다.
-//    }
-//}
-//class 전사{
-//    String name;
-//    String 무기;
-//
-//    void 공격(String name,String 무기){
-//        this.name =name;
-//        this.무기 = 무기;
-//        System.out.println(this.name+"이(가) "+this.무기+"(으)로 공격합니다.");
-//    }
-//    void 재공격(){
-//        System.out.println(this.name+"이(가) "+this.무기+"(으)로 공격합니다.");
-//    }
-//}
