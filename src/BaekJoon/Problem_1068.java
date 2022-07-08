e410f8451c29f405fa6ae9c97dfbf46bd84dfc12
woominsik <9737638@naver.com>
2022-07-08 17:24:38 +0900

    "22-07-08"

--- src/BaekJoon/Problem_1068.java
+++ src/BaekJoon/Problem_1068.java
@@ -1,18 +1,23 @@
 package BaekJoon;
 
-
+/**
+ * 먼저 각 노드들의 자식들을 저장할 HashSet배열인 alSet을 선언하고 각 노드들의 부모가 저장된 arr을 사용하였다.
+ * 먼저 alSet에 arr을 for문을 돌려 바로 밑에 자식들을 저장하고 for문을 통해 리프노드들을 찾았다.
+ * 해당 리프노드들에서 dfs를 돌려 상위 노드에게 자신의 모든 자식들을 넘겨주는 식으로 진행하였다.
+ * 그리고 모든 alSet을 최신화한 후 for문을 통해 deleted이거나 deleted의 자식임을 확인하고 각 Set에서 삭제한다.
+ * 그리고 해당 set의 크기가 0일 경우 answer에 값을 더해 풀었다.
+ */
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 
-
-
-
-
-class Main{
-    static Map<Integer, Integer>map; //num이랑 총 자식 수
+public class Problem_1068 {
+    static Map<Integer, Integer> map; //num이랑 총 자식 수
     static HashSet<Integer>[] alSet; // 자식을 담은 배열
     static int[] arr;
     public static void main(String[] args)throws IOException {
@@ -54,59 +59,12 @@ class Main{
         System.out.println(answer);
     }
     static void dfs(int i){
-       if(arr[i]==-1){
-           return;
-       }
-       else{
-           alSet[arr[i]].addAll(alSet[i]);
-           dfs(arr[i]);
-       }
+        if(arr[i]==-1){
+            return;
+        }
+        else{
+            alSet[arr[i]].addAll(alSet[i]);
+            dfs(arr[i]);
+        }
     }
 }
-
-
-//code.oa.gg/java8/940
-// 문제 : 아래 코드의 잘 못된 점을 고쳐보세요.
-
-//class Main {
-//    public static void main(String[] args) {
-//        자동차 자동차 = new 자동차();
-//        자동차.달리다();
-//    }
-//}
-//
-//class 자동차 {
-//    void 달리다() {
-//        System.out.println("자동차가 달립니다.");
-//    }
-//}
-
-//class Main {
-//    public static void main(String[] args) {
-//        사람 a사람1 = new 사람();
-//        a사람1.이름 = "홍길동";
-//        a사람1.나이 = 22;
-//
-//        a사람1.자기소개();
-//        // 출력 : 저는 22살 홍길동 입니다.
-//
-//        사람 a사람2 = new 사람();
-//        a사람2.이름 = "홍길순";
-//        a사람2.나이 = 25;
-//
-//        a사람2.자기소개();
-//        // 출력 : 저는 25살 홍길순 입니다.
-//
-//        a사람1.자기소개();
-//        // 출력 : 저는 22살 홍길동 입니다.
-//    }
-//}
-//
-//class 사람 {
-//    int 나이;
-//    String 이름;
-//
-//    void 자기소개() {
-//        System.out.println("저는 " + 나이 + "살 " + 이름 + " 입니다.");
-//    }
-//}
