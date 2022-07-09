14befa55f1a063536c0b5e9410a15dbe67894e3d
woominsik <9737638@naver.com>
2022-07-09 13:42:29 +0900

    "22-07-09"

--- src/BaekJoon/Problem_5639.java
+++ src/BaekJoon/Problem_5639.java
@@ -1,18 +1,17 @@
 package BaekJoon;
 
-
+/**
+ * 직접 이진 검색 트리를 구현하여 문제를 해결하였다.
+ * Tree클래스 내부 inputNode에서는 root가 널인지 판별하고 null일 경우 root에 노드를 넣어주고 아닐 경우 findNode함수를 통해
+ * 해당 위치를 찾아 값을 넣어주었다.
+ */
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
 
-
-
-
-
-class Main{
-    public static void main(String[] args) throws IOException{
+public class Problem_5639 {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String input;
         Tree tree = new Tree();
@@ -42,7 +41,7 @@ class Main{
                 findNode(n, this.root);
             }
         }
-        void findNode(int n,Node node){
+        void findNode(int n, Node node){
             if(n<node.num){
                 if(node.left==null){
                     node.left = new Node();
@@ -80,50 +79,3 @@ class Main{
         }
     }
 }
-
-
-//code.oa.gg/java8/940
-// 문제 : 아래 코드의 잘 못된 점을 고쳐보세요.
-
-//class Main {
-//    public static void main(String[] args) {
-//        자동차 자동차 = new 자동차();
-//        자동차.달리다();
-//    }
-//}
-//
-//class 자동차 {
-//    void 달리다() {
-//        System.out.println("자동차가 달립니다.");
-//    }
-//}
-
-//class Main {
-//    public static void main(String[] args) {
-//        사람 a사람1 = new 사람();
-//        a사람1.이름 = "홍길동";
-//        a사람1.나이 = 22;
-//
-//        a사람1.자기소개();
-//        // 출력 : 저는 22살 홍길동 입니다.
-//
-//        사람 a사람2 = new 사람();
-//        a사람2.이름 = "홍길순";
-//        a사람2.나이 = 25;
-//
-//        a사람2.자기소개();
-//        // 출력 : 저는 25살 홍길순 입니다.
-//
-//        a사람1.자기소개();
-//        // 출력 : 저는 22살 홍길동 입니다.
-//    }
-//}
-//
-//class 사람 {
-//    int 나이;
-//    String 이름;
-//
-//    void 자기소개() {
-//        System.out.println("저는 " + 나이 + "살 " + 이름 + " 입니다.");
-//    }
-//}
