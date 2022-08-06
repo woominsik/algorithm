d1db9e88ea8aeee64454362873be4294303560d2
woominsik <9737638@naver.com>
2022-08-07 00:16:39 +0900

    "22-08-05"

--- src/BaekJoon/Problem_1806.java
+++ src/BaekJoon/Problem_1806.java
@@ -1,12 +1,12 @@
 package BaekJoon;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-
-import java.io.*;
-import java.util.*;
-
-public class Main {
-    public static void main(String[] args) throws IOException{
+public class Problem_1806 {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
