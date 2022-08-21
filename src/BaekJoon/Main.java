2ab42548efbdb79606243b2bde55e59a3566d0aa
woominsik <9737638@naver.com>
2022-08-21 16:41:27 +0900

    "22-08-21"

--- src/BaekJoon/Main.java
+++ src/BaekJoon/Main.java
@@ -4,75 +4,8 @@ package BaekJoon;
 
 import java.io.*;
 import java.util.*;
-import java.util.stream.Stream;
 
 public class Main {
 
-    static int N;
-    static int M;
-    static int [][] board;
-    static int [] dx = {1,-1,0,0};
-    static int [] dy = {0,0,1,-1};
-    static int answer = Integer.MAX_VALUE;
-    static boolean [][] visited;
-    public static void main(String[] args) throws IOException{
-        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-
-        int [] temp = Arrays.stream(br.readLine().split(" "))
-                .mapToInt(Integer::parseInt)
-                .toArray();
-
-        N = temp[0];
-        M = temp[1];
-
-        board = new int[N][M];
-        visited = new boolean[N][M];
-
-        for(int i=0;i<N;i++){
-            board[i] = Stream.of(br.readLine().split("")).mapToInt(Integer::parseInt).toArray();
-        }
-
-        dfs(0,0,1,0);
-        if(answer==Integer.MAX_VALUE){
-            System.out.println(-1);
-        }
-        else{
-            System.out.println(answer);
-        }
-
-    }
-    static void dfs(int x, int y, int k,int count){
-        visited[y][x] = true;
-
-        count +=1;
-        if(x==M-1 && y==N-1){
-            if(count<answer)
-                answer = count;
-        }
-
-        if(k>0){
-            for(int i=0;i<4;i++){
-                int tx = x+dx[i];
-                int ty = y+dy[i];
-                if(tx>=0 && tx<M && ty>=0 && ty<N){
-                    if(board[ty][tx]==1 && !visited[ty][tx]){
-                        dfs(tx,ty,k-1,count);
-                        visited[ty][tx] = false;
-                    }
-                }
-            }
-        }
-
-        for(int i=0;i<4;i++){
-            int tx = x+dx[i];
-            int ty = y+dy[i];
-            if(tx>=0 && tx<M && ty>=0 && ty<N){
-                if(board[ty][tx]==0 && !visited[ty][tx]){
-                    dfs(tx,ty,k,count);
-                    visited[ty][tx] = false;
-                }
-            }
-        }
-    }
 }
 
--- src/BaekJoon/Problem_1600.java
+++ src/BaekJoon/Problem_1600.java
@@ -4,85 +4,89 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Problem_1600 {
 
-    static int [] horseDx = {2,2,-2,-2,1,1,-1,-1};
-    static int [] horseDy = {1,-1,1,-1,2,-2,2,-2};
-    static int [][] board;
-    static boolean [][] visited;
-    static int W;
-    static int H;
+    static int K, W, H;
+    static int[][] board;
+    static int min = Integer.MAX_VALUE;
+    static int[] horseDx = {-2, -2, -1, -1, 1, 1, 2, 2}; //말이 이동할 수 있는 8방향
+    static int[] horseDy = {-1, 1, -2, 2, -2, 2, -1, 1};
+    static int[] dx = {0, 1, 0 ,-1}; // 원숭이가 이동할 수 있는 4방향
+    static int[] dy = {1, 0, -1, 0};
+    static boolean[][][] visited;
 
-    static int [] dx = {1,-1,0,0};
-    static int [] dy = {0,0,1,-1};
-    static int answer = Integer.MAX_VALUE;
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int K = Integer.parseInt(br.readLine());
+
+        K = Integer.parseInt(br.readLine());
+
         int [] temp = Arrays.stream(br.readLine().split(" "))
                 .mapToInt(Integer::parseInt)
                 .toArray();
-
         W = temp[0];
         H = temp[1];
 
         board = new int[H][W];
-
-        for(int i=0;i<H;i++){
-            board[i] = Arrays.stream(br.readLine().split(" "))
+        for(int i = 0; i < H; i++) {
+            board [i] = Arrays.stream(br.readLine().split(" "))
                     .mapToInt(Integer::parseInt)
                     .toArray();
         }
 
-        visited = new boolean[H][W];
-        dfs(0,0,K,0);
-        if(answer == Integer.MAX_VALUE){
-            System.out.println(-1);
-        }
-        else{
-            System.out.println(answer-1);
-        }
+        visited = new boolean[H][W][K + 1];
 
+        visited[0][0][K] = true;
+        min = bfs(0, 0);
 
+        if(min == Integer.MAX_VALUE) System.out.println("-1");
+        else System.out.println(min);
     }
-    static void dfs(int x, int y, int k,int count){
-        visited[y][x] = true;
-        count +=1;
-        if(count>answer){
-            return;
-        }
-//        System.out.println(x+" "+y+" "+count);
 
-        if(x==W-1&&y==H-1){
-            if(count<answer)
-                answer = count;
-        }
+    public static int bfs(int x, int y) {
+        Queue<Node> q = new LinkedList<>();
+        q.offer(new Node(x, y, 0, K));
 
-        if(k>0){
-            for(int i=0;i<8;i++){
-                int tx=x+horseDx[i];
-                int ty = y+horseDy[i];
+        while(!q.isEmpty()) {
+            Node current = q.poll();
+            if(current.x == H - 1 && current.y == W - 1) return current.count;
 
-                if(tx>=0 && tx<W && ty>=0 && ty<H){
-                    if(board[ty][tx]==0&&!visited[ty][tx]){
-                        dfs(tx,ty,k-1,count);
-                        visited[ty][tx]=false;
+            for(int i = 0; i < 4; i++) {
+                int nx = current.x + dx[i];
+                int ny = current.y + dy[i];
+                if(nx >= 0 && ny >= 0 && nx < H && ny < W && !visited[nx][ny][current.k] && board[nx][ny] == 0) {
+                    visited[nx][ny][current.k] = true;
+                    q.offer(new Node(nx, ny, current.count + 1, current.k));
+                }
+            }
+
+            if(current.k > 0) {
+                for(int i = 0; i < 8; i++) {
+                    int nx = current.x + horseDx[i];
+                    int ny = current.y + horseDy[i];
+                    if(nx >= 0 && ny >= 0 && nx < H && ny < W && !visited[nx][ny][current.k - 1] && board[nx][ny] == 0) {
+                        visited[nx][ny][current.k - 1] = true;
+                        q.offer(new Node(nx, ny, current.count + 1, current.k - 1));
                     }
                 }
             }
         }
-        for(int i=0;i<4;i++){
-            int tx=x+dx[i];
-            int ty = y+dy[i];
+        return min;
+    }
 
-            if(tx>=0 && tx<W && ty>=0 && ty<H) {
-                if(board[ty][tx]==0&&!visited[ty][tx]){
-                    dfs(tx,ty,k,count);
-                    visited[ty][tx]=false;
-                }
+    public static class Node {
+        int x;
+        int y;
+        int count;
+        int k;
 
-            }
+        public Node(int x, int y, int count, int k) {
+            this.x = x;
+            this.y = y;
+            this.count = count;
+            this.k = k;
         }
     }
 }
